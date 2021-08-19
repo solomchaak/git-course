47eeed0d135f68feafe5c81665d1e1919273bd91
solomchaak <iviksb3@gmail.com>
2021-08-19 18:43:05 +0300

    added gitignore

--- MainTry.java
+++ MainTry.java
@@ -2,10 +2,10 @@ public class MainTry {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
         switch(x) {
-            case x < 1:
+            case 1:
                 System.out.println("SMALLER");
                 break;
-            case x > 1:
+            case 3:
                 System.out.println("BIGGER");
             default:
                 System.out.println("EQUAL");
